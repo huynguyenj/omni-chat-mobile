66cf65f6e271f11eab1c0e39ff2c6dca239ee5b0
huynguyenj <nguyenhuyjobs@gmail.com>
2026-04-06 18:05:49 +0700

    update: finish UI task screen

--- src/screens/staff/TaskScreen.tsx
+++ src/screens/staff/TaskScreen.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import TaskHeader from '@/features/task/components/TaskHeader'
+import TaskList from '@/features/task/components/TaskList'
 
 export default function TaskScreen() {
   return (
-    <View>
-      <Text>TaskScreen</Text>
+    <View style={styles.container}>
+      <TaskHeader/>
+      <TaskList/>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+  }
+})
